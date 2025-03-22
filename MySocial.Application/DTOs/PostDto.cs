df2c125ad967157979a8a2202d47a152cf44efe7
Brener Leite <brenerdealves@gmail.com>
2025-03-21 22:55:46 -0300

    feat: create post dto

---
+++ MySocial.Application/DTOs/PostDto.cs
@@ -0,0 +1,23 @@
+﻿using MySocial.Domain.Entities;
+
+namespace MySocial.Application.DTOs;
+public class PostDto
+{
+    Guid Id { get; set; }
+    string Content { get; set; }
+    DateTime CreatedAt { get; set; }
+    string AuthorName { get; set; }
+    int CommentsCount { get; set; }
+
+    public static explicit operator PostDto(Post post)
+    {
+        return new PostDto
+        {
+            Id = post.Id,
+            Content = post.Content,
+            CreatedAt = post.CreatedAt,
+            AuthorName = post.User.Name,
+            CommentsCount = post.Comments.Count
+        };
+    }
+}
