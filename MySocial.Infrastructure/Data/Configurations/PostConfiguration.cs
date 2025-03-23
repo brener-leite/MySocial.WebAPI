cb7f3c439e66d028b6e98f04475f36f4edf1a599
Brener Leite <brenerdealves@gmail.com>
2025-03-23 20:01:27 -0300

    feat: configure post infra using EF Core

---
+++ MySocial.Infrastructure/Data/Configurations/PostConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using MySocial.Domain.Entities;
+
+namespace MySocial.Infrastructure.Data.Configurations;
+public class PostConfiguration : IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> builder)
+    {
+        builder.ToTable("Posts");
+        builder.HasKey(p => p.Id);
+        builder.HasMany(p => p.Comments)
+            .WithOne(c => c.Post)
+            .HasForeignKey(c => c.PostId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
