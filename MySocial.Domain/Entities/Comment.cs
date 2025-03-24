ba80d7462d26d8b3d013471c5d33a3d6d7314268
Brener Leite <brenerdealves@gmail.com>
2025-03-23 22:50:23 -0300

    fix: adjust comment entity relations

--- MySocial.Domain/Entities/Comment.cs
+++ MySocial.Domain/Entities/Comment.cs
@@ -3,24 +3,37 @@
 public class Comment
 {
     public Guid Id { get; private set; }
-    public Guid PostId { get; private set; }
-    public Guid UserId { get; private set; }
     public string Content { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime? UpdatedAt { get; private set; }
+    public bool IsDeleted { get; private set; }
 
-    public Comment(Guid postId, Guid userId, string content)
+    public Guid PostId { get; private set; }
+    public Post Post { get; private set; }
+
+    public Guid UserId { get; private set; }
+    public User User { get; private set; }
+
+    public Comment(string content, Post post, User user)
     {
         Id = Guid.NewGuid();
-        PostId = postId;
-        UserId = userId;
         Content = content;
+        Post = post;
+        PostId = post.Id;
+        User = user;
+        UserId = user.Id;
         CreatedAt = DateTime.UtcNow;
+        IsDeleted = false;
     }
 
     public void UpdateContent(string content)
     {
         Content = content;
         UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void Delete()
+    {
+        IsDeleted = true;
     }
 }
